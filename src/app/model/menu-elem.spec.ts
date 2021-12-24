b9100292c5327bb539d9e614125a1426e28cde6a
Solange DAGUISE <melodieolivia@gmail.com>
2021-12-24 13:24:21 +0100

    suppression

--- src/app/model/menu-elem.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MenuElem } from './menu-elem';
-
-describe('MenuElem', () => {
-  it('should create an instance', () => {
-    expect(new MenuElem()).toBeTruthy();
-  });
-});
--- src/app/model/menu-elem.ts
+++
@@ -1,4 +0,0 @@
-export class MenuElem {
-    nomMenu! : string;
-    liensMenu! : string;
-}
